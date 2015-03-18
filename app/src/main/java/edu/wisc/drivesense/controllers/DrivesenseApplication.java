49b0b1d7eaf1f2e02af84e6e25d7811c29c37cf5
Damouse <mickey.barboi@gmail.com>
2015-03-18 12:28:53 -0500

    Updated Ubertesters SDK

--- app/src/main/java/edu/wisc/drivesense/controllers/DrivesenseApplication.java
+++ app/src/main/java/edu/wisc/drivesense/controllers/DrivesenseApplication.java
@@ -1,12 +1,16 @@
 package edu.wisc.drivesense.controllers;
 
 import android.app.Application;
+import com.ubertesters.common.models.ActivationMode;
+import com.ubertesters.common.models.LockingMode;
+import com.ubertesters.sdk.Ubertesters;
 import edu.wisc.drivesense.businessLogic.Concierge;
 
 public class DrivesenseApplication extends Application {
 	@Override
 	public void onCreate() {
 	    super.onCreate();
+        Ubertesters.initialize(this, LockingMode.LockApplication, ActivationMode.Widget);
 
         //loads the demo user before anyone needs it. Loads the demo user on first run
 //        Concierge.initializeConcierge();
